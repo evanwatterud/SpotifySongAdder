4ced41c8bec3771802490bf26b41cc023288f73a
evanwatterud <cod.videos@hotmail.com>
2018-03-16 19:16:01 -0400

    tray icon appearing

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const { app, Tray, Menu } = require('electron');
+const path = require('path');
 
+const assetsDir = path.join(__dirname, 'assets')
+let tray = null
+
+app.dock.hide()
+
+app.on('ready', () => {
+  tray = new Tray(path.join(assetsDir, 'spotifyIcon.png')) // Icon made by Elegant Themes
+
+  const contextMenu = Menu.buildFromTemplate(
+    
+  )
+})
