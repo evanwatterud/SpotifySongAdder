88dcf8ec79da8eafdc21b164f5e4f16b970b4e23
evanwatterud <cod.videos@hotmail.com>
2018-03-17 14:38:54 -0400

    added quit option

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, Tray, Menu } = require('electron');
+const { app, Tray, globalShortcut, Menu } = require('electron');
 const path = require('path');
 
 const assetsDir = path.join(__dirname, 'assets')
@@ -7,9 +7,26 @@ let tray = null
 app.dock.hide()
 
 app.on('ready', () => {
+  createTray()
+  registerHotkey()
+})
+
+createTray = () => {
   tray = new Tray(path.join(assetsDir, 'spotifyIcon.png')) // Icon made by Elegant Themes
 
-  const contextMenu = Menu.buildFromTemplate(
-    
-  )
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Quit', role: 'quit', accelerator: 'CommandOrControl+Q'}
+  ])
+
+  tray.setContextMenu(contextMenu)
+}
+
+registerHotkey = () => {
+  globalShortcut.register('CommandOrControl+=', () => {
+    console.log('CommandOrControl+= is pressed')
+  })
+}
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll() // Unregister all hotkeys
 })
